907e82d075021ffdb3a78542927e08e55e663078
AjCanCode <123421198+AjCanCode@users.noreply.github.com>
2024-08-07 00:43:56 -0400

    added function to shuffle multiple choice options to utils script. Added function that gets multiple choice options, and pokemon image to main script

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const pokeImgElement = document.getElementById('pokemonImg');
+let prevPokemonArray = []
+let pokemonName = "";
+const questionElement = document.getElementById('question');
+const choicesContainer = document.getElementById('choices');
 
 
 /*************************************************************************************************/
@@ -12,10 +16,59 @@ async function fetchPokemonData(id){
     return apiData;
 }
 
-//immediate invoked func used to test if "RandomNum" and "fetchPokemonData" functions are working. - success
-(async () => {
+//testing if "RandomNum" and "fetchPokemonData" functions are working to pull random pokemon's data. - Successful, commented out
+    // (async () => {
+    //     let pokeId = RandomNum();
+    //     const poke = await fetchPokemonData(pokeId);
+    //     pokeImgElement.src = poke.sprites.other.home.front_default;
+    //     console.log("functions are working!");
+    // })()
+
+
+// get pokemon ID and create multiple choice. "RandomNum()" function is util.js
+async function createChoices(){
     let pokeId = RandomNum();
-    const poke = await fetchPokemonData(pokeId);
-    pokeImgElement.src = poke.sprites.other.home.front_default;
-    console.log("functions are working!");
-})()
+
+    while(prevPokemonArray.includes(pokeId)){
+        pokeId = RandomNum()
+        }
+    prevPokemonArray.push(pokeId);
+    const pokemon = await fetchPokemonData(pokeId);
+    
+    const choicesName = [pokemon.name];
+    const choicesId = [pokemon.id];
+
+    // for loop to pull 3 additional random pokemon for mulitple choice. should have 4 choices total.
+        for (let i = choicesName.length; choicesName.length < 4; i++){
+        let randPokeId = RandomNum();
+
+        while(choicesId.includes(randPokeId)){
+            randPokeId = RandomNum();
+        }
+        choicesId.push(randPokeId);
+
+        const randPokemon = await fetchPokemonData(randPokeId);
+        const randPokemonName = randPokemon.name;
+        choicesName.push(randPokemonName);
+
+        pokemonName = choicesName[0];
+
+}
+// call "shuffleArr" function, found in Util.js
+shuffleArr(choicesName);
+
+//reset question and pokeImg elements
+questionElement.textContent = "Who's That Pokemon?";
+pokeImgElement.src = pokemon.sprites.other.home.front_default;
+
+//  create multiple choice buttons and event for option chosen by user
+choicesContainer.innerHTML = '';
+choicesName.forEach((option, index)=> {
+    const button = document.createElement("button");
+button.textContent = option;
+button.onclick = (event) => checkAns(option === pokemon.name, event);
+choicesContainer.appendChild(button);
+});
+}
+
+createChoices();
--- utils.js
+++ utils.js
@@ -1,6 +1,16 @@
 // these functions are used for the "WHOSE THAT POKEMON QUIZ"
 
+
 //get random number limited to first 5 generations of pokemon.
 function RandomNum() {
-    return Math.floor(Math.random() * 650) + 1;
+    return Math.floor(Math.random() * 649) + 1;
+}
+
+//shuffle array function that uses the Fisher-Yates Algo
+function shuffleArr(array){
+    for (let i = array.length - 1; i > 0; i--){
+        const rand = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[rand]] =[array[rand], array[i]]
+    }
 }
