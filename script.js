e1f9cde6081b7f3d1d11a92e73f6bef5d9bfc01c
AjCanCode <123421198+AjCanCode@users.noreply.github.com>
2024-08-07 02:02:13 -0400

    completed quiz, added styling to quiz, added additional functions to util script, updated links

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ let prevPokemonArray = []
 let pokemonName = "";
 const questionElement = document.getElementById('question');
 const choicesContainer = document.getElementById('choices');
+let missed = 0;
+let score = 0;
+const scoreElement = document.getElementById('Value');
+const missedTotal = document.getElementById('missed');
+let gameStatus = "go"
+let tier = '';
+const body = document.getElementById('body');
+const scoreimg = document.getElementById('score');
+
 
 
 /*************************************************************************************************/
 // FEATURE #4 Retrieve data from a third-party API and use it to display something within your app.
 /*************************************************************************************************/
-
 // Get pokemon data from PokeAPI by using pokemon's ID number
 async function fetchPokemonData(id){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -25,7 +33,12 @@ async function fetchPokemonData(id){
     // })()
 
 
-// get pokemon ID and create multiple choice. "RandomNum()" function is util.js
+
+/**************************************************************************************************************/
+// FEATURE #1 Use arrays, objects, sets or maps to store and retrieve information that is displayed in your app.
+/**************************************************************************************************************/   
+
+// get pokemon ID and create multiple choice options by storing the data in arrays. The "RandomNum()" function is in util.js
 async function createChoices(){
     let pokeId = RandomNum();
 
@@ -61,6 +74,11 @@ shuffleArr(choicesName);
 questionElement.textContent = "Who's That Pokemon?";
 pokeImgElement.src = pokemon.sprites.other.home.front_default;
 
+
+/**********************************************************************************************************************/
+// FEATURE #2. Analyze data that is stored in arrays, objects, sets or maps and display information about it in your app.
+/**********************************************************************************************************************/
+
 //  create multiple choice buttons and event for option chosen by user
 choicesContainer.innerHTML = '';
 choicesName.forEach((option, index)=> {
@@ -71,4 +89,73 @@ choicesContainer.appendChild(button);
 });
 }
 
+/****************************************************************************************************/
+// FEATURE #3 Create a function that accepts two or more input parameters and 
+//returns a value that is calculated or determined by the inputs.
+/****************************************************************************************************/
+
+// check answer function
+function checkAns(rightAnswer, event){
+    const chosenButton = document.querySelector('.chosen');
+
+    if (chosenButton) {
+        return;
+    }
+    event.target.classList.add("chosen");
+ 
+    if (rightAnswer){
+        displayAnswer(pokemonName);
+        score++;
+        scoreElement.textContent = score;
+        event.target.classList.add('correct');
+        pokeImgElement.classList.add('correct');
+        backGroundTier();
+        if ( missed >  2){
+            gameStatus = "done";
+        }
+        
+    } else if(!rightAnswer){
+        
+        missed++;
+        missedTotal.textContent = missed;
+        event.target.classList.add("wrong");
+        displayWrong("Wrong Pokemon");
+        if ( missed > 2){
+            gameStatus = "done"
+        }
+    }
+
+//  insert a .75s delay before next question
+if(gameStatus === 'go'){
+setTimeout(()=>{
+    event.target.classList.remove('correct');
+    pokeImgElement.classList.remove("correct");
+    createChoices();
+
+}, 750);
+} else{
+   const answer = confirm("You got "+score + " Right" + "!\nPlay Again?");
+
+   //reset quiz if user wants to play again
+   if (answer){
+    gameStatus= 'go';
+    prevPokemonArray = [];
+    missed = 0;
+    score = 0;
+    missedTotal.textContent = missed;
+    scoreElement.textContent = score;
+
+        //reset tier if needed
+        if(tier != ''){
+        scoreimg.classList.remove(tier);
+        body.classList.remove(tier);
+        }
+    createChoices();
+
+   }
+}
+
+}
+
+// create multiple choice
 createChoices();
--- utils.js
+++ utils.js
@@ -14,3 +14,41 @@ function shuffleArr(array){
         [array[i], array[rand]] =[array[rand], array[i]]
     }
 }
+
+  // Show correct answer
+  function displayAnswer(result) {
+    questionElement.textContent = "It's " + result + "!";
+  }
+
+  // show answer is wrong pokemon
+  function displayWrong() {
+    questionElement.textContent = "Wrong Pokemon!";
+  }
+
+  // change background and score when certain score is met
+  function backGroundTier(){
+    if (score >= 25){
+        scoreimg.classList.remove('masterball');
+        body.classList.remove('masterball');
+        scoreimg.classList.add('gsball');
+        body.classList.add('gsball');
+        tier = 'gsball';
+    } else if (score >= 15) {
+        scoreimg.classList.remove('ultraball');
+        body.classList.remove('ultraball');
+        scoreimg.classList.add('masterball');
+        body.classList.add('masterball');
+        tier = 'masterball';
+    } else if (score >= 8) {
+        scoreimg.classList.remove('greatball');
+        body.classList.remove('greatball');
+        scoreimg.classList.add('ultraball');
+        body.classList.add('ultraball');
+        tier = 'ultraball';
+
+    } else if (score >= 4){
+        scoreimg.classList.add('greatball');
+        body.classList.add('greatball');     
+        tier = 'greatball' ;
+    }
+}
